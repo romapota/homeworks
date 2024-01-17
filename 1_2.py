e9c07204557dc532145844fffbbad31e4c156d72
romapota <154024023+romapota@users.noreply.github.com>
2024-01-17 17:25:04 +0300

    Update 1_2.py

--- 1_2.py
+++ 1_2.py
@@ -8,16 +8,16 @@ def print_hi(name):
     m = []
     p = 0
     p_p = 0
-    for i in range(1, len(str(number))+1):
+    for i in range(1, len(str(number))+1):#вычисление каждой цифры числа
         m.append(((number%(10**i))-p)//10**(i-1))
         p += ((number%(10**i))-p)
     new_number = ''
-    for i in m:
+    for i in m:#переворот числа
         new_number += str(i)
     new_number = int(new_number)
-    if negative_b == 1:
+    if negative_b == 1:#если число было отрицательным, то домножение на -1
         new_number *= -1
-    if (int(new_number) >= -128) and (int(new_number) <= 127):
+    if (int(new_number) >= -128) and (int(new_number) <= 127):#сравнение
         return print(new_number)
     else:
         return print('not solution')
