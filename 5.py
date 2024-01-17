f8c78806cc209e58de67377c1977609909acbb99
romapota <154024023+romapota@users.noreply.github.com>
2024-01-17 17:52:36 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,5 +1,5 @@
 from itertools import product
-def get_pins(count):
+def get_pins(count: int) -> int:
     banks = []
     cashs = []
     it = []
@@ -12,16 +12,16 @@ def get_pins(count):
         banks.append(one)
         cashs.append(cash)
 
-    if cashs[0] > cashs[1]:
+    if cashs[0] > cashs[1]:#если сумма денег в первом банке больше, то пусть сумма второго банка равна первому
         it.append(banks[0])
         cashs[1] = cashs[0]
 
     for i in range(2, len(cashs)):
-        if cashs[i] + cashs[i-2] > cashs[i-1]:
+        if cashs[i] + cashs[i-2] > cashs[i-1]:#если сумма денег в третьем банке и в первом больше, чем во втором, то количество денег третьего банка равна сумме третьего и первого 
             cashs[i] = cashs[i]+cashs[i-2]
             it.append(banks[i])
         else:
-            cashs[i] = cashs[i-1]
+            cashs[i] = cashs[i-1]#иначе пусть количевство денег в третьем банке равно количеству денег во втором
     print(max(cashs))
 
 
