489e7f1f14e6b1f1f4b1f9a0e422c8bfa765fa8c
romapota <154024023+romapota@users.noreply.github.com>
2024-01-17 17:33:40 +0300

    Update 2_2.py

--- 2_2.py
+++ 2_2.py
@@ -1,20 +1,20 @@
 def print_hi(name):
     s = str(input())
-    s.lower()
+    s.lower()#все символы преобразовавывает в маленькие
     m = []
     str_n = ''
     for i in s:
         if i != ' ':
-            str_n += i
+            str_n += i#отделяем слова
         else:
             if str_n != '':
-                m.append(str_n.strip())
+                m.append(str_n.strip())#добавляем другие строки удаляем пробелы 
             str_n = ''
             if i != ' ':
                 str_n += i
     m.append(str_n)
-    m.reverse()
-    n_m = ' '.join(m).capitalize()
+    m.reverse()#переворот слов
+    n_m = ' '.join(m).capitalize()#склеивание и первое слово с большой буквы
     m = n_m.strip()
     return print(m)
 
