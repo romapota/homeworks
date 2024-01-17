5cb7e9e8d0b8e1c1ab402145c9b5a2b3a982d4d8
romapota <154024023+romapota@users.noreply.github.com>
2024-01-17 17:48:50 +0300

    Update 4_3.py

--- 4_3.py
+++ 4_3.py
@@ -2,12 +2,12 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-def sorted_word(word):
+def sorted_word(word: str) -> str:
     m = [i for i in word]
-    m_n = sorted(m)
-    new_word = ''.join(m_n)
+    m_n = sorted(m)#сортировка массива
+    new_word = ''.join(m_n)#создание строки
     return new_word
-def print_hi(name):
+def print_hi(name: str) -> list:
     m = []
     m_n = []
     t = []
@@ -18,9 +18,9 @@ def print_hi(name):
     for i in m:
         for j in m:
             if len(i) == len(j):
-                n_i = sorted_word(i)
+                n_i = sorted_word(i)#сортировка слов
                 n_j = sorted_word(j)
-                if n_j == n_i:
+                if n_j == n_i:#если слова равны, то добавляется в список
                     t.append(j)
         if t not in m_n:
             m_n.append(t)
