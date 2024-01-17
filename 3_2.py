04e8be02b61bb7ff93b9634547c27ba186225132
romapota <154024023+romapota@users.noreply.github.com>
2024-01-17 17:43:02 +0300

    Update 3_2.py

--- 3_2.py
+++ 3_2.py
@@ -1,5 +1,5 @@
 import numpy
-def get_pins():
+def get_pins() -> list:
     matrix = []
     matrix_v = []
     s = int(input('Столбцов: '))
@@ -10,15 +10,15 @@ def get_pins():
         matrix.append(matrix_v)
         matrix_v = []
     print(matrix)
-    if len(matrix[0]) > len(matrix):
+    if len(matrix[0]) > len(matrix):#если столбцов больше, чем строк, то дополняется нулями
         for i in range(len(matrix[0])-len(matrix)):
             matrix.append([0 for _ in range(len(matrix[0]))])
-    if len(matrix) > len(matrix[0]):
+    if len(matrix) > len(matrix[0]):#если строк больше, чем столбцов, то дополняется нулями
         for i in range(len(matrix)):
             for j in range(len(matrix)-len(matrix[-1])):
                 matrix[i].append(0)
     matrix_nn = numpy.array(matrix)
-    matrix_r = numpy.rot90(matrix_nn)
+    matrix_r = numpy.rot90(matrix_nn)#поворот матрицы на 90 градусов
     return print(matrix_r)
 
 # Press the green button in the gutter to run the script.
