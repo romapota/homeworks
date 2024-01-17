e98d9d5bfdf88378387f864c84c890669a308409
romapota <154024023+romapota@users.noreply.github.com>
2024-01-17 17:56:50 +0300

    Update 7_1.py

--- 7_1.py
+++ 7_1.py
@@ -1,7 +1,7 @@
-def santa_users(name):
+def santa_users(name:int) -> dict:
     s = []
     nameSurnameIndex = []
-    for i in range(name):
+    for i in range(name):#заполнение данных о пользователях
         name = str(input())
         index = str(input())
         s.append(name)
@@ -9,7 +9,7 @@ def santa_users(name):
         nameSurnameIndex.append(s)
         s = []
     for i in nameSurnameIndex:
-        if i[1] == '':
+        if i[1] == '':#если вместо индекса пусто, то добавляется None
             i[1] = 'None'
     return print(dict(nameSurnameIndex))
 
