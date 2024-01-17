70c49eab051d44e25235f1505810895412d6ae4d
romapota <154024023+romapota@users.noreply.github.com>
2024-01-17 17:28:20 +0300

    Update 1_3.py

--- 1_3.py
+++ 1_3.py
@@ -9,14 +9,14 @@ def get_pins():
     flag_one = 0
     flag_two = 0
     for i in range(count):
-        sp.append(['0' for _ in range(len(s))])
-    for i in s:
+        sp.append(['0' for _ in range(len(s))])#создаем массив и заполняем нулями
+    for i in s:#заполнение массива буквами с помощью индексов
         if flag_one == 0:
             indexTop += 1
-        if indexTop == len(sp):
+        if indexTop == len(sp): если число дошло до низа, то первый флаг меняется на 1(далее будет заполнение снизу вверх)
             flag_one = 1
             indexTop -= 1
-        if flag_one == 1:
+        if flag_one == 1:#индекс верха и права меняются на 1
             if indexTop != 0:
                 indexTop -= 1
                 indexRight += 1
@@ -27,7 +27,7 @@ def get_pins():
         sp[indexTop][indexRight] = i
     for i in range(len(sp)):
         for j in range(len(sp[0])):
-            if sp[i][j] != '0':
+            if sp[i][j] != '0':#вывод чисел, если если отличаются от нуля(пустового значения)
                 ss += str(sp[i][j])
     return print(ss)
 
