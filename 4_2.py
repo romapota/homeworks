218fb664344c46c7acf1c929ae73f81b7faf2285
romapota <154024023+romapota@users.noreply.github.com>
2024-01-22 12:09:21 +0300

    Update 4_2.py

--- 4_2.py
+++ 4_2.py
@@ -1,22 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+import itertools
 def print_hi(count) -> list:
+    m: list
+    m_all: list
+    m_new: list
     m = []
     m_all = [m,]
     for i in range(count):
         r = int(input())
         m.append(r)
-    for i in m:
-        for j in m:
-            for s in m:
-                a = [i, j, s]
-                if (a not in m_all) and (i != j != s) and (i!=j) and (i!=s) and (s!=j):#заполнение возможных перестановок, если такой еще не существует в массиве
-                    m_all.append(a)
-    return print(m_all)
+    m_all = list(itertools.permutations(m))#находим все перестановки
+    m_new = []
+    for i in m_all:
+        if [x for x in i] not in m_new:#если такой перестановки нет в новом списке, то добавляем ее
+            m_new.append([x for x in i])
+    return print(m_new)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi(int(input('Количество чисел: ')))
