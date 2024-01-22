283f2a667f1a14f8795bc128f62c3da487951bd2
romapota <154024023+romapota@users.noreply.github.com>
2024-01-22 11:29:43 +0300

    Update 2_1.py

--- 2_1.py
+++ 2_1.py
@@ -1,16 +1,23 @@
-def print_hi(name) -> int:
+def print_hi(name)->str:
+    s: str
+    m: list
+    max_i : str
     s = str(input())
     m = []
     str_n = ''
     for i in s:
-        if i not in str_n:#если буква уникальна, то добавляется
-            str_n += i
-        else:#иначе временный список сбрасывается и начинается новый
+        if str(i) not in str_n:#если такая буква не входит в строку, то она добавляется
+            str_n += str(i)
+        else:#если входит, то строка добавляется в массив и обнуляется
             m.append(str_n)
             str_n = ''
             str_n += i
     m.append(str_n)
-    return print(max(m))#нахождение максимальной подстроки
+    max_i = ''
+    for i in m:
+        if len(i) > len(max_i):#берем за самый длинный элемент пустую строку и если новый элемент длинее, то меняем значение в переменной на новое
+            max_i = i
+    return print(max_i)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
