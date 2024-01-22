d0de56a81d011c60de1ae4a6782a54b5ac4ccb0a
romapota <154024023+romapota@users.noreply.github.com>
2024-01-22 12:28:42 +0300

    Update 6_1.py

--- 6_1.py
+++ 6_1.py
@@ -1,37 +1,45 @@
 def print_hi(name):
-    def one():
+    m1: list
+    m2: list
+    R: int
+    R2: int
+    numbers: tuple
+    r: int
+    def one():#перебираем элементы каждого массива, если значения одного массива есть в другом, то добавляем в список
         for i in m1:
             if i in m2:
                 numbers.add(i)
         for i in m2:
             if i in m1:
                 numbers.add(i)
-        return print(len(numbers), 'элемента: ', [i for i in numbers])#подсчет элементов в обоих списках
-    def two():
+        return print(len(numbers), 'элемента: ', [i for i in numbers])
+    def two():#перебираем элементы каждого массива, если значения одного массива нет в другом, то добавляем в список
         for i in m1:
             if i not in m2:
                 numbers.add(i)
         for i in m2:
             if i not in m1:
                 numbers.add(i)
-        return print(len(numbers), 'элемента: ', [i for i in numbers])#подсчет элементов, присутствующих только в одном списке
-    def three():
+        return print(len(numbers), 'элемента: ', [i for i in numbers])
+    def three():#перебираем элементы первого массива, если значения первого массива нет во втором, то добавляем в список
         for i in m1:
             if i not in m2:
                 numbers.add(i)
-        return print(len(numbers), 'элемента: ', [i for i in numbers])#) количество оставшихся элементов в list1 после извлечения элементов из list2
-    def four():
+        return print(len(numbers), 'элемента: ', [i for i in numbers])
+    def four():#перебираем элементы второго массива, если значения втрого массива нет в первом, то добавляем в список
         for i in m2:
             if i not in m1:
                 numbers.add(i)
-        print(len(numbers), 'элемента: ', [i for i in numbers])) # количество оставшихся элементов в list1 после извлечения элементов из list2
+        print(len(numbers), 'элемента: ', [i for i in numbers])
     R = int(input('Количество элементов в первом списке '))
     R2 = int(input('Количество элементов во втором списке '))
     m1 = set()
     m2 = set()
+    print('Заполнение первого списка')
     for i in range(R):
         r = int(input())
         m1.add(r)
+    print('Заполнение второго списка')
     for i in range(R2):
         r = int(input())
         m2.add(r)
