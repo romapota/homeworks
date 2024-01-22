ed6283c0d7c464901e9271cd1b7ad796a0aa5f9d
romapota <154024023+romapota@users.noreply.github.com>
2024-01-23 02:45:17 +0300

    Add files via upload

---
+++ 8_1.py
@@ -0,0 +1,86 @@
+import matplotlib.pyplot as plt
+from math import sin, cos
+from sympy import *
+
+x_max = 5 / 10 + 0.1
+x: list
+y: list
+x1_list: list
+y1_list: list
+x2_list: list
+y2_list: list
+x_kas: list
+y_kas: list
+x_norm: list
+y_norm: list
+x_kas_2: list
+y_kas_2: list
+x_mo: float
+y_mo: float
+z_x: float
+y_z: float
+delx: int
+def main():
+    x1, y1 = symbols('x1 y1')
+    function = 'sin(x)*cos(x**2+5)'
+    x_max = 5/10 + 0.1
+    x = []
+    y = []
+    x1_list = []
+    y1_list = []
+    x2_list = []
+    y2_list = []
+    x_kas = []
+    y_kas = []
+    x_norm = []
+    y_norm = []
+    x_kas_2 = []
+    y_kas_2 = []
+    x_mo = 0.3
+    y_mo = 0.101
+    for delx in range(0, 5+1):#значения для графика функции
+        x_z =+ delx/10
+        y_z = sin(x_z)*cos(x_z**2+5)
+        x.append(x_z)
+        y.append(y_z)
+    for delx in range(0, 5+1):#значения для график первой производной
+        x_z =+ delx/10
+        y_z = -2*x_z*sin(x_z)*sin(x_z**2 + 5) + cos(x_z)*cos(x_z**2 + 5)
+        x1_list.append(x_z)
+        y1_list.append(y_z)
+    for delx in range(0, 5+1):#значения для график второй производной
+        x_z =+ delx/10
+        y_z = -4*x_z**2*sin(x_z)*cos(x_z**2 + 5) - 4*x_z*sin(x_z**2 + 5)*cos(x_z) - 2*sin(x_z)*sin(x_z**2 + 5) - sin(x_z)*cos(x_z**2 + 5)
+        x2_list.append(x_z)
+        y2_list.append(y_z)
+    for delx in range(0, 5+1):#значения для касательной в т. (0.3, 0.101)
+        x_z =+ delx/10
+        y_z = (sin(x_mo)*cos(x_mo**2+5)) + ((-2*x_mo*sin(x_mo)*sin(x_mo**2 + 5) + cos(x_mo)*cos(x_mo**2 + 5))*(x_z-x_mo))
+        x_kas.append(x_z)
+        y_kas.append(y_z)
+    for delx in range(0, 5+1):#значения для нормали в т. (0.3, 0.101)
+        x_z =+ delx/10
+        y_z = (sin(x_mo)*cos(x_mo**2+5)) - ((1/(-2*x_mo*sin(x_mo)*sin(x_mo**2 + 5) + cos(x_mo)*cos(x_mo**2 + 5)))*(x_z-x_mo))
+        x_norm.append(x_z)
+        y_norm.append(y_z)
+    for x_zn in range(0, 6):
+        x_zh_z = x_zn/10
+        for delx in range(0, 5 + 1):  # значения для касательной в т. (0.3, 0.101)
+            x_z = + delx / 10
+            y_z = (sin(x_zh_z) * cos(x_zh_z ** 2 + 5)) + ((-2 * x_zh_z * sin(x_zh_z) * sin(x_zh_z ** 2 + 5) + cos(x_zh_z) * cos(x_zh_z ** 2 + 5)) * (x_z - x_zh_z))
+            x_kas_2.append(x_z)
+            y_kas_2.append(y_z)
+            plt.plot(x_kas_2, y_kas_2, color='orange')  # график касательной
+    plt.plot(x, y, color = 'green')#график функции
+    plt.plot(x1_list, y1_list, color = 'blue')#первая производная
+    plt.plot(x2_list, y2_list, color = 'red')#график второй производной
+    plt.plot([min(x)], [min(y)], 'o', color = 'b')#точка с минимальным значением фукнции
+    plt.plot([max(x)], [max(y)], 'o', color='b')#точка с максимальным значением функции
+    plt.plot(x_kas, y_kas, linestyle = '--', color = 'k')#график касательной
+    plt.plot(x_norm, y_norm, linestyle = '--', color = 'k')#график нормали
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ 8_2.py
@@ -0,0 +1,118 @@
+import matplotlib.pyplot as plt
+from math import sin, cos
+from sympy import *
+import pylab
+from matplotlib.widgets import Button, Slider, RadioButtons, CheckButtons
+
+colors: dict
+styles: dict
+x_kas: list
+y_kas: list
+x_new: int
+x_old: int
+x: list
+y: list
+check_flag: bool
+button_flag: bool
+flag_update: bool
+def main():
+    x_kas = []
+    y_kas = []
+    def addGraph(graph_axes, x, y, x_point, y_point):
+        graph_axes.plot(x, y)
+        graph_axes.plot(x_point, y_point, 'o', color='k')
+    def updateGraph():#обновление графика
+        global check_flag, button_flag, radiobuttons_color, radiobuttons_style, flag_update, x_old
+        colors = {'Красный': 'r', 'Синий': 'b', 'Зеленый': 'g'}
+        styles = {'Сплошная': '-', 'Пунктир': '--', 'Точки': 'dotted'}
+        color = colors[radiobuttons_color.value_selected]
+        style = styles[radiobuttons_style.value_selected]
+        graph_axes.clear()
+        if flag_update:
+            x_new = slider_x.val
+            x_old = x_new
+            flag_update = False
+        if flag_update == False:
+            x_new = x_old
+        if button_flag:
+            x_new = 0
+            button_flag = False
+        if check_flag:
+            graph_axes.clear()
+            y_new = sin(x_new) * cos(x_new ** 2 + 5)
+            for delx in range(0, 5 + 1):  # значения для касательной в т. (0.3, 0.101)
+                x_z = + delx / 10
+                y_z = (sin(x_new) * cos(x_new ** 2 + 5)) + ((-2 * x_new * sin(x_new) * sin(x_new ** 2 + 5) + cos(x_new) * cos(x_new ** 2 + 5)) * (x_z - x_new))
+                x_kas.append(x_z)
+                y_kas.append(y_z)
+            graph_axes.clear()
+            graph_axes.plot(x, y, color = color, linestyle = style)
+            graph_axes.plot(x_new, y_new, 'o', color='k')
+            graph_axes.plot(x_kas, y_kas, color = 'k')
+        if check_flag == False:
+            graph_axes.clear()
+            y_new = sin(x_new) * cos(x_new ** 2 + 5)
+            graph_axes.plot(x, y, color = color, linestyle = style)
+            graph_axes.plot(x_new, y_new, 'o', color='k')
+        pylab.draw()
+    def onChangeGraph(value):
+        global flag_update
+        flag_update = True
+        updateGraph()
+    def onCheckClicked(label):
+        global check_flag
+        if label == 'Касательная':
+            check_flag = not check_flag
+        updateGraph()
+    def onButtonClickes(label):
+        global button_flag
+        button_flag = True
+        updateGraph()
+    def onRadioButtonsClicked_color(label):
+        updateGraph()
+    def onRadioButtonsClicked_style(label):
+        updateGraph()
+    global check_flag, button_flag, radiobuttons_color, radiobuttons_style, flag_update, x_old
+    check_flag = False
+    button_flag = False
+    flag_update = False
+    x_old = 0
+    x_max = 5/10 + 0.1
+    xx = 0
+    x = []
+    y = []
+    for delx in range(0, 5+1):#значения для графика функции
+        x_z =+ delx/10
+        y_z = sin(x_z)*cos(x_z**2+5)
+        x.append(x_z)
+        y.append(y_z)
+
+    fig, graph_axes = pylab.subplots()
+    graph_axes.grid()
+    addGraph(graph_axes, x, y, 0.3, 0.1)
+
+    fig.subplots_adjust(left=0.07, right=0.95, top=0.95, bottom=0.55)
+
+    axes_slider_x = plt.axes([0.05, 0.35, 0.85, 0.04])
+    slider_x = Slider(axes_slider_x, label = 'x', valmin=0, valmax=max(x), valinit=0)#создание слайдера
+    slider_x.on_changed(onChangeGraph)#отслеживание изменения слайдера
+    axes_checkbuttons = plt.axes([0.0, 0.15, 0.2, 0.1])
+    check_button_kasat = CheckButtons(axes_checkbuttons, ['Касательная'], [False])
+    check_button_kasat.on_clicked(onCheckClicked)
+    axes_button_add = plt.axes([0.7, 0.05, 0.25, 0.075])
+    button_delete = Button(axes_button_add, 'Сбросить')
+    button_delete.on_clicked(onButtonClickes)
+    axes_radiobuttons = plt.axes([0.25, 0.05, 0.2, 0.2])
+    radiobuttons_color = RadioButtons(axes_radiobuttons,['Красный', 'Синий', 'Зеленый'])
+    radiobuttons_color.on_clicked(onRadioButtonsClicked_color)
+    axes_radiobuttons_two = plt.axes([0.45, 0.05, 0.2, 0.2])
+    radiobuttons_style = RadioButtons(axes_radiobuttons_two,['Сплошная', 'Пунктир', 'Точки'])
+    radiobuttons_color.on_clicked(onRadioButtonsClicked_style)
+
+    updateGraph()
+    pylab.show()
+
+
+
+if __name__ == '__main__':
+    main()
