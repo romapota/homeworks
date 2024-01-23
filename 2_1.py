7ffc66d79b4484efad6f1f9095c822e98b56d080
romapota <154024023+romapota@users.noreply.github.com>
2024-01-23 21:23:09 +0300

    Update 2_1.py

--- 2_1.py
+++ 2_1.py
@@ -5,14 +5,16 @@ def print_hi(name)->str:
     s = str(input())
     m = []
     str_n = ''
-    for i in s:
-        if str(i) not in str_n:#если такая буква не входит в строку, то она добавляется
-            str_n += str(i)
-        else:#если входит, то строка добавляется в массив и обнуляется
-            m.append(str_n)
-            str_n = ''
-            str_n += i
-    m.append(str_n)
+    for x in range(len(s)):
+        for i in s[x:]:
+            if str(i) not in str_n:#если такая буква не входит в строку, то она добавляется
+                str_n += str(i)
+            else:#если входит, то строка добавляется в массив и обнуляется
+                m.append(str_n)
+                str_n = ''
+                str_n += i
+        m.append(str_n)
+        str_n = ''
     max_i = ''
     for i in m:
         if len(i) > len(max_i):#берем за самый длинный элемент пустую строку и если новый элемент длинее, то меняем значение в переменной на новое
